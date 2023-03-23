cb900fbde2ebab9bd69ffd7193ce0da6d09bbc61
Víctor Hugo Carreón Pulido <hv_carreon@hotmail.com>
2023-03-23 15:52:16 -0600

    Correcciones de Cursor.sql

--- Practica NOMITEC/Cursor.sql
+++ Practica NOMITEC/Cursor.sql
@@ -16,16 +16,23 @@ as $$
     Declare idEmp integer;
     Declare sueldoEmp numeric(18,2);
     Declare idCC integer;
+    Declare totSueldo numeric(18,2);
 
     Declare tmpId integer;
     Declare fechaAct timestamp := cast(now() as timestamp);
-    Declare totSueldo numeric(18,2);
 
     --Para la conexion
-    Declare conn text := 'host=localhost port=49153 dbname=bddnomina user=postgres password=postgrespw';
     Declare state text;
 
 Begin
+
+    --Insertar en nomina
+    vRNomina:= (coalesce((Select max(idnomina) from nomina),0)+1);
+
+    insert into nomina (numeronomina, fechainicio, fechafin, aplicada)
+        values (vRNomina, pFechaInicio,pFechaFin,B'0');
+
+    vRNomina:=(Select max(idnomina) from nomina);
 
     --Proceso de Insertar en Nomina_Detalle
     open insertarNomDetalle;
@@ -33,19 +40,19 @@ Begin
             fetch next from insertarNomDetalle into idEmp, sueldoEmp, idCC;
             exit when not FOUND;
 
+            totSueldo:= (sueldoEmp+vComplementos-vDeducciones);
+
             insert into nomina_detalle (idnomina, idempleado, sueldobase, totaldeducciones, totalcomplementos, totalsueldo)
-                values (vRNomina, idEmp,sueldoEmp,vDeducciones,vComplementos,((sueldoEmp+vComplementos) - vDeducciones));
+                values (vRNomina, idEmp,sueldoEmp,vDeducciones,vComplementos,totSueldo);
 
             --Proceso de Insertar en Poliza
             state:=dblink_exec('dbname=contabilidad user=postgres password=postgrespw',
                 'INSERT INTO public.tb_con_poliza (numero_poliza, fecha_poliza, saldo_deudor, saldo_acreedor, aplicada, tipo_poliza)
-                values ('''||cast(vRNomina as varchar(25))||''','''||fechaAct||''','''||sueldoEmp||''','''||sueldoEmp||''',B''0'',''B'')');
+                values ('''||cast(vRNomina as varchar(25))||''','''||fechaAct||''','''||totSueldo||''','''||totSueldo||''',B''0'',''B'')');
 
             --Recupera el id de Poliza que acabas de insertar
             tmpId := (Select temp.id from dblink('dbname=contabilidad user=postgres password=postgrespw',
                 'SELECT max(numero_poliza) from public.tb_con_poliza') as temp (id integer));
-
-            totSueldo:= (Select distinct totalsueldo from nomina_detalle where idnomina = tmpId and idempleado = idEmp);
 
             --Insertar en Poliza_Detalle dos registros
             --Registro Uno
@@ -56,7 +63,7 @@ Begin
             --Registro Dos
             state:=dblink_exec('dbname=contabilidad user=postgres password=postgrespw','
                 insert into public.tb_con_poliza_detalle (id_poliza, id_cuenta_contable, debe, haber, referencia)
-                    values ('||tmpId||', '||idCC||', ''0'','''||totsueldo||''', ''De nomina'')');
+                    values ('||tmpId||', ''103'', ''0'','''||totsueldo||''', ''De nomina'')');
 
         end loop;
     close insertarNomDetalle;
